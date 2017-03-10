705cc06eba3e4cb3be02d40ec8bf701b4e2012d8
Ayoub Serti <ayb.sert@gmail.com>
2017-03-10 18:14:17 +0000

    choose server and solution; start solution on server; eval on server

--- main.js
+++ main.js
@@ -2,14 +2,87 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 const {ipcMain} = require('electron')
+const {evalOnServer} = require('./lib/eval-on-server.js');
+
+
+
+const {dialog, Menu} = require('electron')
+const {exec} = require('child_process')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let currentSolutionPath
+let currentServerPath
 
-function createWindow () {
+const menus = [
+  {
+    label: 'solution',
+    submenu: [
+      {
+        label: 'Open Solution',
+        click() {
+          dialog.showOpenDialog({
+            properties: ['openFile'],
+            title: 'choose Wakanda Solution'
+            //filters  :[{ name : "wakanda-enterprise-server"}]
+          }, function (l) {
+            if (l !== undefined && l.length > 0) {
+              currentSolutionPath = l[0];
+            }
+
+          });
+        }
+      },
+      {
+        label: 'Start Solution',
+        click() {
+          if (currentSolutionPath && currentServerPath) {
+            exec(currentServerPath + " " + currentSolutionPath);
+          }
+        }
+
+      }
+    ]
+
+  },
+  {
+    label: "Server",
+    submenu: [
+      {
+        label: "choose",
+        click() {
+          dialog.showOpenDialog({
+            properties: ['openFile'],
+            title: 'choose Wakanda Server',
+            filters: [{ name: "Wakanda Server", extentions: [".exe"] }]
+          }, function (l) {
+            if (l !== undefined && l.length > 0) {
+              currentServerPath = l[0];
+              win.webContents.send("server-choose", currentServerPath);
+            }
+
+          });
+        }
+      },
+      {
+        label: 'open debugger',
+        click() {
+          require('electron').shell.openExternal('http://127.0.0.1:8080/debugger')
+        }
+      }
+
+
+    ]
+  }
+]
+
+
+
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +91,7 @@ function createWindow () {
     slashes: true
   }))
 
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
@@ -26,15 +99,32 @@ function createWindow () {
     // when you should delete the corresponding element.
     win = null
   })
+  const menu = Menu.buildFromTemplate(menus)
+  Menu.setApplicationMenu(menu)
 }
 
 //register ipcMain msg handler
-ipcMain.on("eval-on-server",(event, arg) => {
+ipcMain.on("eval-on-server", (event, arg) => {
+  evalOnServer(arg, function (err, data) {
+    
+    event.sender.send("eval-done", data.toString());
+  })
 
-    console.log(arg);
-    event.sender.send("eval-done", true);
 })
-  
+
+ipcMain.on("choose-server", (event, arg) => {
+
+  dialog.showOpenDialog({
+    properties: ['openFile'],
+    title: 'choose Wakanda Server'
+    //filters  :[{ name : "wakanda-enterprise-server"}]
+  }, function (l) {
+    if (l !== undefined && l.length > 0)
+      event.sender.send("server-choose", l[0]);
+  });
+
+})
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -55,6 +145,7 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
+
 })
 
 // In this file you can include the rest of your app's specific main process
